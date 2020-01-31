c81fca42d119809d935fc7e91cd0372be944dbf5
Bessa, Anis <anis.bessa@ext.saint-gobain.com>
2020-01-31 19:23:37 +0100

    Fix Validation and define the advice of RestStarter as prior one

--- easyms-secured-sample-ms/src/main/java/com/easyms/sampleapp/model/entity/Client.java
+++ easyms-secured-sample-ms/src/main/java/com/easyms/sampleapp/model/entity/Client.java
@@ -1,34 +1,22 @@
 package com.easyms.sampleapp.model.entity;
 
-import com.easyms.sampleapp.utils.SampleAppMessages;
-import lombok.*;
+import lombok.Builder;
+import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Builder
-@ToString()
 public class Client
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter @Setter
     private Long id;
-    @NotNull
-    @Getter @Setter
+
     private String firstname;
-    @NotNull
-    @Getter @Setter
+
     private String lastname;
-    @NotNull
-    @Email(
-            message = SampleAppMessages.INVALID_EMAIL
-    )
-    @Getter @Setter
     @Column(unique = true)
     private String email;
 }
